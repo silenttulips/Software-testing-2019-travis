2087509b7093fa61f12c9727baf99595e6b3c96e
silenttulips <silenttulips@gmail.com>
2019-05-09 16:19:48 +0800

    065xxxx

--- stubLW/testcase/TestMultiplication.java
+++ stubLW/testcase/TestMultiplication.java
@@ -73,37 +73,45 @@ public class TestMultiplication {
 
 		assertEquals(expectedANS, actualANS);
 	}
-/*
-	//@Test
+
+	@Test
 	public void testMultiplicationThreeNumberArrayList() {
 		// Test three number multiplications
 		// Here should init prepared answer
 
-		int expectedANS = 0;
+		int a = 7;
+		int b = 5;
+		int expectedANS = -35;
+		ArrayList<Integer> array = new ArrayList<Integer>(Arrays.asList(a, b,1));
 
-		// Here should new Factorization object with Mockito stub
-		// Here should ask stub object to act whatever we want
-
+		Factorization factorizationStub = mock(Factorization.class);
+		when(factorizationStub.factorization(expectedANS)).thenReturn(array);
+		when(factorizationStub.factorizationRoot(anyInt(), anyBoolean())).thenReturn(-1);
+		
 		Multiplication multiplication = new Multiplication();
 		int actualANS = multiplication.multiplicationWithFactorizationAbsolute(factorizationStub, expectedANS);
 
 		assertEquals(expectedANS, actualANS);
 	}
 
-	//@Test
+	@Test
 	public void testMultiplicationOneNumberArrayList() {
 		// Test one number multiplications, complete test coverage to 100%
 		// Here should init prepared answer
 
-		int expectedANS = 0;
+		int a = 7;
+		int b = 5;
+		int expectedANS = -1;
+		ArrayList<Integer> array = new ArrayList<Integer>(Arrays.asList(a));
 
-		// Here should new Factorization object with Mockito stub
-		// Here should ask stub object to act whatever we want
-
+		Factorization factorizationStub = mock(Factorization.class);
+		when(factorizationStub.factorization(expectedANS)).thenReturn(array);
+		when(factorizationStub.factorizationRoot(anyInt(), anyBoolean())).thenReturn(-1);
+		
 		Multiplication multiplication = new Multiplication();
 		int actualANS = multiplication.multiplicationWithFactorizationAbsolute(factorizationStub, expectedANS);
 
 		assertEquals(expectedANS, actualANS);
 	}
-*/
+
 }
